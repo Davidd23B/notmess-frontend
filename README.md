7a228d9b567d79e4c1d82db0ca5e27265f9a1f22
David <davidburillo28@gmail.com>
2025-11-24 20:21:36 +0100

    Base del proyecto NotMess-Frontend

---
+++ README.md
@@ -0,0 +1,3 @@
+# notmess_frontend
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/notmess_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.notmess_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
